90b70447693ef515abfcf19265cceced50416834
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-21 17:09:17 -0500

    getId TDD working

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -10,4 +10,19 @@ public class Student {
         this.name = name;
     }
 
+    // returns the student's id
+    public long getId() {
+        return id;
+    }
+//    // // returns the student's name
+//    public String getName() {...}
+//
+//    // adds the given grade to the grades list
+//    public void addGrade(int grade){...}
+//
+//    // returns the list of grades
+//    public ArrayList<Integer> getGrades(){...}
+//
+//    // returns the average of the students grades
+//    public double getGradeAverage(){...}
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,13 @@
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.*;
+
+
 public class StudentTest {
+    private Student student = new Student(123345, "Scott");
+    @Test
+    public void testGetId () {
+        assertEquals("should test the id", 123345, student.getId());
+    }
+
 }
