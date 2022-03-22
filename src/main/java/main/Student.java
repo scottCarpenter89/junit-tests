ac6c5a888368754f017a63bebdcc55b52ba98f96
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-22 09:55:01 -0500

    refactored testGetName

--- src/main/java/main/Student.java
+++ src/main/java/main/Student.java
@@ -3,14 +3,27 @@ package main;
 import java.util.ArrayList;
 
 public class Student {
-private long id;
-private String name;
-private ArrayList<Integer> numbers;
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
 
     public Student(long id, String name) {
         this.id = id;
         this.name = name;
     }
+
+    public Student(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
     public long getId() {
         return id;
@@ -18,5 +31,9 @@ private ArrayList<Integer> numbers;
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public void addGrade(int grade){
+
     }
 }
--- src/test/java/main/StudentTest.java
+++ src/test/java/main/StudentTest.java
@@ -3,25 +3,28 @@ package main;
 import main.Student;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 
 public class StudentTest {
+    Student scott = new Student(12345, "Scott");
 
 
     @Test
     public void testGetId() {
-        Student scott = new Student(12345, "Scott");
+
         assertEquals("should test the id", 12345, scott.getId());
     }
 
-//    @Test
-//    public void testGetName() {
-//        assertEquals("tests the get name method", "Scott", student.getName());
-//    }
-//
+    @Test
+    public void testGetName() {
+        assertEquals("tests the get name method", "Scott", scott.getName());
+    }
+
 //    @Test
 //    public void testAddGrade() {
-//        assertArrayEquals(, );
+//        assertArrayEquals();
 //    }
 }
